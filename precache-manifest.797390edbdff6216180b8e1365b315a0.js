de3ee0f1c0046e1e7f48f9c9d784e518452cc4a3
skornkir <skornkir@icloud.com>
2019-02-17 19:07:30 +0300

    Updates

---
+++ precache-manifest.797390edbdff6216180b8e1365b315a0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/kostikova/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "b79309d732c9c9378ee4",
+    "url": "/kostikova/static/css/main.501a43f8.chunk.css"
+  },
+  {
+    "revision": "8cfa5519c2148a1e585a15186d229cc2",
+    "url": "/kostikova/static/media/user-profile.8cfa5519.jpg"
+  },
+  {
+    "revision": "229c2c55cd3e4e3999b4",
+    "url": "/kostikova/static/js/1.229c2c55.chunk.js"
+  },
+  {
+    "revision": "b271972bb8f4e723d7f7",
+    "url": "/kostikova/static/js/runtime~main.b271972b.js"
+  },
+  {
+    "revision": "303c75e00f7fb2b2ab2fc18ab60b1b8c",
+    "url": "/kostikova/static/media/bg3.303c75e0.png"
+  },
+  {
+    "revision": "b79309d732c9c9378ee4",
+    "url": "/kostikova/static/js/main.b79309d7.chunk.js"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/kostikova/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/kostikova/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/kostikova/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "229c2c55cd3e4e3999b4",
+    "url": "/kostikova/static/css/1.be0bf932.chunk.css"
+  },
+  {
+    "revision": "9ba6b9ea1a6e0b76cf9886aa266d7873",
+    "url": "/kostikova/index.html"
+  }
+];
